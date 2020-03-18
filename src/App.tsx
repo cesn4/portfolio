c02a4b261e49a73bc9c1d5c9f78ec7527ea0ee22
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-03-18 17:31:08 +0200

    Terminal tested switch working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
-import React, { FunctionComponent } from 'react';
-import TerminalInput from './components/Terminal';
+import React, { FunctionComponent, useState } from 'react';
+import Terminal from 'terminal-in-react';
+
+import './app.scss';
 
 const App: FunctionComponent = () => {
+    const [answerState, setanswerState] = useState('');
+    const aiCall: Function = (command: commandType) => {
+        switch (command) {
+            case 'Good morning':
+                return 'Hell there, how can I help you?';
+            case 'Who are you?':
+                return 'Why do you care?';
+            default:
+                return null;
+        }
+    };
+    console.log(answerState);
     return (
         <div>
-            <TerminalInput />
+            <div className="answer">{aiCall(answerState)}</div>
+            <div className="terminal">
+                <div className="terminal__title">Terminal</div>
+                <Terminal
+                    description={{ color: false, show: false, clear: false }}
+                    prompt="gray"
+                    color="white"
+                    backgroundColor="black"
+                    style={{ fontSize: '30px' }}
+                    commandPassThrough={(cmd): string =>
+                        `${cmd}: command not found`
+                    }
+                    msg="You can write anything here. Example - Hello! My name is Foo and I like Bar."
+                    allowTabs={false}
+                    showActions={false}
+                    commands={{
+                        gm: (): void => {
+                            setanswerState('Good morning');
+                        },
+                        hu: (): void => {
+                            setanswerState('Who are you?');
+                        },
+                    }}
+                />
+            </div>
         </div>
     );
 };
+
+type commandType = 'Good morning' | 'Who are you?';
 
 export default App;
