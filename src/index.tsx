79642ba4e3626a94b546281e0a17aa171d26bdde
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-03-19 09:57:31 +0200

    Provider imported

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
 
 import '~/styles/styles.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+);
