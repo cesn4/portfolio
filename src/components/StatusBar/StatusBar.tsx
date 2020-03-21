803ea37b84918f881290556cc0efbd91ce399eb3
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-03-21 14:14:23 +0200

    colors added to Ai state

--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -6,11 +6,21 @@ const StatusBar: FunctionComponent<StatusBarProps> = ({
     state,
 }: StatusBarProps) => {
     const className = 'status-bar';
+    const availableText = 'Available ';
     const renderStatus: Function = (aiState: boolean) => {
         if (aiState) {
-            return 'Available for Work';
+            return (
+                <span className={`${className}__online-state`}>
+                    <span className={`${className}__online-state-available`}>
+                        {availableText}
+                    </span>
+                    for Work
+                </span>
+            );
         } else {
-            return 'Offline';
+            return (
+                <span className={`${className}__offline-state`}>Offline</span>
+            );
         }
     };
     console.log(renderStatus(state));
@@ -18,7 +28,7 @@ const StatusBar: FunctionComponent<StatusBarProps> = ({
         <div className={className}>
             <span className={`${className}__id`}>ID: mc94 portfolio AI</span>
             <span className={`${className}__status`}>
-                Status: {renderStatus(state)}
+                Status: {renderStatus(true)}
             </span>
         </div>
     );
